fa98987505eaaec907ca757a3008a674fc903c36
Tomas Mawyin <tmawyin@gmail.com>
2015-06-30 18:38:40 -0400

    Added functionality to read from CSV file and display data

--- CopaAmerica2015/copaAmerica.js
+++ CopaAmerica2015/copaAmerica.js
@@ -23,31 +23,72 @@ d3.xml("field.svg", function(xml) {
 		.style("display", "block")
 		.style("margin","auto");
 
-	var xVals = [990,1008,1030.5,811.8];
-	var yVals = [360,315,328.5,382.5];
-	var texto = ["CH","CH","U","E"];
-	var colors = ["red","red","blue","yellow"]
+	// Loading the CSV file and parsing through the data
+	d3.csv("ca2015.csv", function(d) {
+  		return {
+		    team: d.Team, // convert "Year" column to Date
+		    opp: d.Against,
+		    player: d.Player,
+		    round: +d.Round,
+		    color: d.Color,
+		    game: +d.Game,
+		    x: +d.x_true,
+		    y: +d.y_true,
+		    label: d.Team.substring(0,2).toUpperCase()
+  			};
+		}, 
+		// Function that uses the data 
+		function(error, rows) {
+			var circle = svg.selectAll("g")
+				.data(rows)
+				.enter()
+				.append("circle")
+				.attr("cx", function(d){return d.x;} )
+				.attr("cy",function(d){return d.y;})
+				.attr("r",12)
+				.style("fill",function(d){return d.color;});
 
-	var circle = svg.selectAll("g")
-			.data(xVals)
-			.enter()
-			.append("circle")
-			.attr("cx", function(d){return d;} )
-			.attr("cy",function(d,i){return yVals[i];})
-			.attr("r",12)
-			.style("fill",function(d,i){return colors[i];});
-
-	var lables = svg.selectAll("text")
-				.data(texto)
+			var lables = svg.selectAll("text")
+				.data(rows)
 				.enter()
 				.append("text")
 				.attr("text-anchor", "middle")
-				.attr("x", function(d,i){return xVals[i];})
-				.attr("y",function(d,i){return yVals[i]+3.5;})
-				.text(function(d){return d;})
+				.attr("x", function(d){return d.x;})
+				.attr("y",function(d){return d.y+3.5;})
+				.text(function(d){return d.label;})
 				.style("font-family","Arial")
 				.style("fill","black")
 				.style("stroke","black")
 				.style("font-size","10px");
+	});
 
 });
+
+/* TESTING CODE ------
+	// var xVals = [990,1008,1030.5,811.8,162];
+	// var yVals = [360,315,328.5,382.5,216];
+	// var texto = ["CH","CH","U","E","A"];
+	// var colors = ["red","red","DodgerBlue","yellow","DeepSkyBlue"]
+
+	// var circle = svg.selectAll("g")
+	// 		.data(xVals)
+	// 		.enter()
+	// 		.append("circle")
+	// 		.attr("cx", function(d){return d;} )
+	// 		.attr("cy",function(d,i){return yVals[i];})
+	// 		.attr("r",12)
+	// 		.style("fill",function(d,i){return colors[i];});
+
+	// var lables = svg.selectAll("text")
+	// 			.data(texto)
+	// 			.enter()
+	// 			.append("text")
+	// 			.attr("text-anchor", "middle")
+	// 			.attr("x", function(d,i){return xVals[i];})
+	// 			.attr("y",function(d,i){return yVals[i]+3.5;})
+	// 			.text(function(d){return d;})
+	// 			.style("font-family","Arial")
+	// 			.style("fill","black")
+	// 			.style("stroke","black")
+	// 			.style("font-size","10px");
+	*/
